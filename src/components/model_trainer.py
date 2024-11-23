e18d7fab6335f00e8a7a159005fc98acb373c468
Akashraiden <akasht2098@gmail.com>
2024-11-23 17:23:21 +0530

    ----->

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -20,7 +20,7 @@ from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+    trained_model_file_path = os.path.join("Data_files", "model.pkl")
 
 
 class ModelTrainer:
